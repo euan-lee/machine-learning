3e49cf624d4fe9a4384a19930c9a16502380b5b9
이범진 (euan) <62791913+euan-lee@users.noreply.github.com>
2022-03-18 13:44:32 +0900

    Update 1week.py

--- Datavisualization/1week.py
+++ Datavisualization/1week.py
@@ -1,6 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 f = open("data.txt",'r')
-line = f.readline()
-print(line)
+w1=float(input())
+w2=float(input())
+setha=float(input())
+#y = w1/w2*x+setha
+print(w1)
+x1=[]
+x2=[]
+y=[]
+
+plt.xlabel('X1-Axis')
+plt.ylabel('X2-Axis')
+
+plt.grid(True)
+while True:
+    line = f.readline()
+    if not line: break
+    haha=line.split()
+    x1.append(haha[0])
+    x2.append(haha[1])
+    
+
+for i in x1:
+    y.append(w1/w2*float(i)+setha/w2)   
+    
+print(y)    
+plt.scatter(x1,x2)
+plt.plot(x1,y);
+plt.show()
 f.close()
