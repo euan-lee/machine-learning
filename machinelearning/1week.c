4ca581badb833b975544bb51ec4189f06128c61b
이범진 (euan) <62791913+euan-lee@users.noreply.github.com>
2022-03-20 01:32:26 +0900

    버그 완전 수정하지 않음 (데이터 분석하고 다시보기)

--- machinelearning/1week.c
+++ machinelearning/1week.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 //텍스트 파일 읽기
 //읽은 함수 데이터에 집어 넣기
 //결과값 y 아웃풋 데이터에 넣기
@@ -7,29 +6,14 @@
 double Sum=0.0;
 double w1=0.0;
 double w2=0.0;
-double setha=0.0;
-
+double setha=0.1;
 struct data{
     double x1;
     double x2;
 };
-//
-
-
-int F_layer_perceptron(float x1,float x2,float w1,float w2,float setha){
-    printf("first layer perceptron");
-    Sum=x1*w1+x2*w2;
-    if(Sum>=1){
-        printf("1\n");
-        return 1;    
-    }
-    else{
-        printf("0\n"); 
-        return 0;
-    }
-  
-}
-
+int flag=0;
+int one=1;
+int zero=0;
 void input_data_on_text_and_file_open(){
     FILE *fp;
     fp = fopen("data.txt", "w");//없으면 만듦
@@ -50,7 +34,6 @@ void input_data_on_text_and_file_open(){
      fclose(fp);
 }
 
-
 void input_w1_w1_setha(){
         printf("plz put w1 and w2");
         //입력값 범위 조정해줘잉
@@ -58,9 +41,26 @@ void input_w1_w1_setha(){
         printf("w1,w2,setha is %f %f %f",w1,w2,setha);
     }
 
+int perceptron(double x1,double x2,double w1,double w2,double setha){
+    printf("x1:%lf x2:%lf w1:%lf w2:%lf setha:%lf\n",x1,x2,w1,w2,setha);
+    Sum=w1*x1+w2*x2;
+    printf("sum is %lf\n",Sum);
+    if(Sum>setha){
+        flag=1;
+        printf("flag:%d",flag);
+    }
+    else {
+        flag=0;
+        printf("flag is %d",flag);
+    }
+}
+
+
+
+
+
 void read_file_and_find_y(){
-     
-    FILE *fp_2= fopen("result.txt", "w");//없으면 만듦
+    FILE *fp2= fopen("result.txt", "w");//없으면 만듦
     FILE *fp = fopen("data.txt", "r");    // hello.txt 파일을 읽기 모드(r)로 열기. 
     struct data data;
     if(fp == NULL){
@@ -68,27 +68,28 @@ void read_file_and_find_y(){
     }
     else{
         printf("file open success (for read)\n");
-        while(!((fscanf(fp, "%lf %lf",&data.x1,&data.x2))==EOF)){
-            if(F_layer_perceptron(data.x1,data.x2,w1,w2,setha)){
-                 fprintf(fp_2," %d\n",1);
-            }else{
-                fprintf(fp_2," %d\n",0);
-            }
-            
+        while(!((fscanf(fp, "%lf %lf",&data.x1,&data.x2))==EOF)){ 
+            printf("result is%d\n",perceptron(data.x1,data.x2,w1,w2,setha));                 
+         if(flag==1){
+            fprintf(fp2," %d\n",flag);
+        }
+        else{
+            fprintf(fp2," %d\n",flag);
+        }
         }
         fclose(fp);
-        fclose(fp_2);
+        fclose(fp2);
     }                                
     
 }
 
 int main (){
-
     input_data_on_text_and_file_open();
   
     input_w1_w1_setha();
     
     read_file_and_find_y();
+      
 }
 
 
