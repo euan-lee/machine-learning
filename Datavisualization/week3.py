a8a9d10df6495018704f4cd27191dbfd77e12969
이범진 (euan) <62791913+euan-lee@users.noreply.github.com>
2022-04-01 13:43:23 +0900

    Update week3.py

--- Datavisualization/week3.py
+++ Datavisualization/week3.py
@@ -9,22 +9,27 @@ x2=[]
 err=[]
 xmin,xmax,ymin,ymax,zmin,zmax = -20, 20, -20, 20, -20,20
 cmin,cmax=0,0
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(111, projection='3d')
 
 while True:
     line = f_w1w2.readline()
     if not line: break
     haha=line.split()
-    x1.append(float(haha[0]))
-    x2.append(float(haha[1]))
+    x1.append([float(haha[0])])
+    x2.append([float(haha[1])])
     
+
 while True:
     line = f_err.readline()
     if not line: break
-    err.append(float(line))
-    
-fig = plt.figure(figsize=(6, 6))
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(x1,x2,err, c="black", marker='o', s=15, cmap='Greens')
+    err.append([float(line)])
+for number in range(len(err)):  
+    if err[number][0]==0.0:    
+        print(err[number][0])
+        ax.scatter(x1[number][0],x2[number][0],err[number][0], c="red", marker='o', s=15, cmap='Greens')       
+    else:
+        ax.scatter(x1[number][0],x2[number][0],err[number][0], c="black", marker='o', s=15, cmap='Greens')     
 
 plt.show() 
 f_w1w2.close()
